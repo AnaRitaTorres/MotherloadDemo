e5e68e7c9f79f1430e71ad798bc35a5d7281763a
dalugoga <dalugoga@gmail.com>
2016-05-06 22:45:20 +0100

    first commit

---
+++ desktop/src/com/dalugoga/mother/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dalugoga.mother.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dalugoga.mother.MotherloadDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MotherloadDemo(), config);
+	}
+}
